0da1c34f74ecbf0ee2472e766144c605fe512a05
omar lamine <76263000+omarlamin01@users.noreply.github.com>
2022-09-01 01:47:52 +0000

    Update currencyInput.vue

--- src/components/currencyInput.vue
+++ src/components/currencyInput.vue
@@ -1,11 +1,12 @@
 <template>
-    <div class="input">
+    <div id="input">
         <div class="left-box">
             <span id="currency-name">{{ name }}</span>
             <span id="currency-country">{{ country }}</span>
         </div>
         <div class="right-box">
-            <input type="number" v-model="model">
+            <!-- eslint-disable-next-line -->
+            <input type="number" v-model="value">
         </div>
     </div>
 </template>
@@ -15,22 +16,58 @@
 export default 
 {
     name: "currencyInput",
-    data() 
-    {
-        return 
-        {
-            model: 0
-        };
-    },
     props: {
         name: String,
         country: String,
         countryFlag: String,
+        value: Float64Array,
     },
 };
 
 </script>
 
 <style scoped>
-
+#input {
+    margin: 8px;
+    padding: 8px;
+    display: flex;
+    flex-direction: row;
+    background-color: #212121;
+    color: white;
+    border-radius: 9px;
+}
+.left-box {
+    display: flex;
+    flex-direction: column;
+    margin-left: 16px;
+}
+.left-box #currency-name {
+    color: white;
+    font-size: 32px;
+    font-weight: 500;
+    text-align: start;
+    justify-content: flex-start;
+    margin-left: 0;
+}
+.left-box #currency-country {
+    color: rgba(255, 255, 255, 0.8);
+    font-size: 24px;
+    font-weight: 300;
+    text-align: start;
+    justify-content: flex-start;
+    margin-left: 0;
+}
+.right-box {
+    display: flex;
+    flex-direction: column;
+    margin-left: 16px;
+    margin-right: 16px;
+}
+.right-box input[type="number"] {
+    margin-right: 0;
+    margin-left: auto;
+    text-align: end;
+    height: 100%;
+    width: 100%;
+}
 </style>
