13903ff992dca839250a50c3be2c0d3d1d2db71e
omar lamine <76263000+omarlamin01@users.noreply.github.com>
2022-09-01 00:54:29 +0000

    Update currencyInput.vue

---
+++ src/components/currencyInput.vue
@@ -0,0 +1,24 @@
+<template>
+    <div class="input">
+
+    </div>
+</template>
+
+<script>
+
+export default 
+{
+    name: "currencyInput",
+    data() 
+    {
+        return 
+        {
+
+        };
+    }
+}
+</script>
+
+<style scoped>
+
+</style>
