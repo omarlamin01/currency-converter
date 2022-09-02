1dc1c8ad74a197ce4741a9c81ac6f011b93ff04a
omar lamine <76263000+omarlamin01@users.noreply.github.com>
2022-09-02 14:19:12 +0000

    format numbers

--- src/App.vue
+++ src/App.vue
@@ -5,23 +5,60 @@
 		<h1>Currency converter</h1>
 		</header>
 		<main>
-			<div v-for="(value, key) in activeCurrencies" :key="key">
-				<currencyInput 
-					class="currencyInput" 
-					:name="activeCurrencies[key].name" 
-					:country="activeCurrencies[key].country" 
-					:countryFlag="activeCurrencies[key].countryFlag"
-				>
-					<!-- eslint-disable-next-line -->
-					<template slot="input">
+			<!------------------------------>
+			<div class="converter">
+				<div class="input">
+					<div class="left-box">
+						<span class="currency-name">{{ activeCurrenciy1.name }}</span>
+						<span class="currency-country">{{ activeCurrenciy1.country }}</span>
+					</div>
+					<div class="right-box">
 						<input
 							type="number"
-							v-model="inputValue"
+							min="0"
+							v-model="currencyInput1"
+							@change="converterA"
 						>
-					</template>
-					
-				</currencyInput>
+					</div>
+				</div>
+				<div class="drop-down">
+					<div
+						class="element"
+						v-for="currency in currenciesList"
+					>
+						<span class="currency-name">{{ currency.name }}</span>
+						<span class="currency-country"> - ({{ currency.country }})</span>
+					</div>
+				</div>
 			</div>
+			<!------------------------------>
+			<div class="converter">
+				<div class="input">
+					<div class="left-box">
+						<span id="currency-name">{{ activeCurrenciy2.name }}</span>
+						<span id="currency-country">{{ activeCurrenciy2.country }}</span>
+					</div>
+					<div class="right-box">
+						<input 
+							type="number"
+							min="0"
+							maxlength="4"
+							v-model="currencyInput2"
+							@change="converterB"
+						>
+					</div>
+				</div>
+				<div class="drop-down">
+					<div
+						class="element"
+						v-for="currency in currenciesList"
+					>
+						<span class="currency-name">{{ currency.name }}</span>
+						<span class="currency-country"> - ({{ currency.country }})</span>
+					</div>
+				</div>
+			</div>
+			
 		</main>
   	</div>
 </template>
@@ -36,22 +73,46 @@ export default {
 	data() {
 		return {
 			currenciesList: data.currencies,
-			activeCurrencies: data.activeCurrencies,
-			inputValue:  0,
+			activeCurrenciy1: data.currencies[0],
+			activeCurrenciy2: data.currencies[1],
+			currencyInput1:  10,
+			currencyInput2:  1,
 		};
 	},
-	methods: {},
+	methods: {
+		formatNumber(number, digits) {
+			let floor = parseInt(number);
+			let float = parseInt((number - floor) * Math.pow(10, digits))
+			if (float < Math.pow(10, (digits - 1))) {
+				return (floor + '.' + float + '0');
+			} else {
+				return (floor + '.' + float);
+			}
+		},
+		converterA() {
+			this.currencyInput1 = this.formatNumber(this.currencyInput1, 2);
+			this.currencyInput2 = this.formatNumber((this.activeCurrenciy2.valueToUSD * (this.currencyInput1 / this.activeCurrenciy1.valueToUSD)), 2);
+		},
+		converterB() {
+			this.currencyInput2 = this.formatNumber(this.currencyInput2, 2);
+			this.currencyInput1 = this.formatNumber((this.activeCurrenciy1.valueToUSD * (this.currencyInput2 / this.activeCurrenciy2.valueToUSD)), 2);
+		}
+	},
 	components: {
 		currencyInput
 	},
-	watch: {},
-	created() {
-		if (this.activeCurrencies.length < 2) {
-			this.activeCurrencies = [];
-			this.activeCurrencies.push(this.currenciesList[0]);
-			this.activeCurrencies.push(this.currenciesList[1]);
+	watch: {
+		converterA(currencyInput1) {
+			this.currencyInput2 = activeCurrenciy2.valueToUSD * (this.currencyInput1 / activeCurrenciy1.valueToUSD);
+			console.log(this.currencyInput2);
+		},
+		converterB(currencyInput2) {
+			this.currencyInput1 = activeCurrenciy1.valueToUSD * (this.currencyInput2 / activeCurrenciy2.valueToUSD);
 		}
-		
+	},
+	created() {
+		this.currencyInput1 = this.formatNumber(this.currencyInput1, 2);
+		this.currencyInput2 = this.formatNumber(this.currencyInput2, 2);
 	},
 	};
 </script>
@@ -80,8 +141,60 @@ main {
 	align-items: center;
 	justify-content: center;
 }
-.currencyInput {
+.input {
+    margin: 8px;
+    padding: 8px;
+    display: flex;
+    flex-direction: row;
+    background-color: #212121;
+    color: white;
+    border-radius: 9px;
+    justify-content: space-between;
 	min-width: 580px;
 	max-width: 580px;
+}
+.left-box {
+    display: flex;
+    flex-direction: column;
+    margin-left: 16px;
+	cursor: pointer;
+}
+.currency-name {
+    color: white;
+    font-size: 32px;
+    font-weight: 500;
+    text-align: start;
+    justify-content: flex-start;
+    margin-left: 0;
+}
+.currency-country {
+    color: rgba(255, 255, 255, 0.7);
+    font-size: 24px;
+    font-weight: 300;
+    text-align: start;
+    justify-content: flex-start;
+    margin-left: 0;
+}
+.right-box {
+    display: flex;
+    flex-direction: column;
+    margin-left: 16px;
+    margin-right: 16px;
+    width: 50%;
+}
+.right-box input[type="number"] {
+    margin-right: 0;
+    margin-left: auto;
+    text-align: end;
+    font-size: 32px;
+    color: white;
+    outline: none;
+    border: none;
+    background: transparent;
+    height: 100%;
+    width: 50%;
+}
+.element {
+	background-color: #212121;
 }
 </style>
--- src/components/currencyInput.vue
+++ src/components/currencyInput.vue
@@ -1,14 +1,6 @@
 <!-- eslint-disable -->
 <template>
-    <div id="input">
-        <div class="left-box">
-            <span id="currency-name">{{ name }}</span>
-            <span id="currency-country">{{ country }}</span>
-        </div>
-        <div class="right-box">
-            <slot name="input" />
-        </div>
-    </div>
+    
 </template>
 
 <script>
@@ -27,54 +19,5 @@ export default
 </script>
 
 <style scoped>
-#input {
-    margin: 8px;
-    padding: 8px;
-    display: flex;
-    flex-direction: row;
-    background-color: #212121;
-    color: white;
-    border-radius: 9px;
-    justify-content: space-between;
-}
-.left-box {
-    display: flex;
-    flex-direction: column;
-    margin-left: 16px;
-}
-.left-box #currency-name {
-    color: white;
-    font-size: 32px;
-    font-weight: 500;
-    text-align: start;
-    justify-content: flex-start;
-    margin-left: 0;
-}
-.left-box #currency-country {
-    color: rgba(255, 255, 255, 0.8);
-    font-size: 24px;
-    font-weight: 300;
-    text-align: start;
-    justify-content: flex-start;
-    margin-left: 0;
-}
-.right-box {
-    display: flex;
-    flex-direction: column;
-    margin-left: 16px;
-    margin-right: 16px;
-    width: 50%;
-}
-.right-box input[type="number"] {
-    margin-right: 0;
-    margin-left: auto;
-    text-align: end;
-    font-size: 32px;
-    color: white;
-    outline: none;
-    border: none;
-    background: transparent;
-    height: 100%;
-    width: 50%;
-}
+
 </style>
